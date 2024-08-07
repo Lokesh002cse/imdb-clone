2cff41b1e67c151b6f2444189634600b28f88111
Lokesh002cse <lokeshkmr995@gmail.com>
2024-08-08 00:40:30 +0530

    app detail

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { Helmet } from 'react-helmet'; // Import Helmet for setting the page title
 import './App.css';
 
 function Detail() {
@@ -11,8 +12,11 @@ function Detail() {
     }
 
     return (
-        <div className="detail-page">  {/* Changed className to match CSS */}
-            <h1>{movie.name}</h1>
+        <div className="detail-page">
+            <Helmet>
+                <title>{movie.name} - Movie App</title>
+            </Helmet>
+            <h1 className="DETAILS PAGE">{movie.name}</h1> {/* Added title */}
             <img src={movie.image} alt={movie.name} />
             <p className="description">{movie.description}</p>
         </div>
